47a05a3f993d4ed6b28a6e0abf2aef9f2b93b654
Alberto Hernandez <alberto@albertoX1>
2017-09-19 15:01:51 +0200

    export a filtry

--- toc_ntk/src/main/ng-client/src/app/home/home.component.ts
+++ toc_ntk/src/main/ng-client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+//import { FormBuilder } from '@angular/forms';
+import { MzBaseModal, MzModalComponent } from 'ng2-materialize';
 
 import { AppState } from '../app.state';
 import { AppService } from '../app.service';
@@ -13,52 +14,61 @@ import { Candidate } from '../models/candidate';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('exportModal') exportModal: MzModalComponent;
+  @ViewChild('tocModal') tocModal: MzModalComponent;
   toc_text: string;
 
   candidates: Candidate[] = [];
+  selected: Candidate[] = [];
+  exported: string;
   loading: boolean = false;
+  hasToc: boolean = false;
+  
+  showMatched: boolean = true;
+  showFree: boolean = true;
 
   scoreConfig: ScoreConfig = new ScoreConfig();
 
   constructor(
     public state: AppState,
-    private service: AppService,
-    private fb: FormBuilder) {
+    private service: AppService) {
     this.createForm();
   }
 
   createForm() {
-//    sc.dicts = [['PSH', '653_klicova_slova_b.txt']];
-//    this.scoreConfig = this.fb.group(sc);
+    //    sc.dicts = [['PSH', '653_klicova_slova_b.txt']];
+    //    this.scoreConfig = this.fb.group(sc);
   }
   ngOnInit() {
     this.state.stateChanged.subscribe(st => {
       this.analyze();
     });
   }
+  
+  select(){
+    
+    this.selected = this.candidates.filter((c: Candidate) => { return c['selected'] !== 'undefined' && c['selected'] });
+  }
 
   analyze() {
     this.loading = true;
     this.candidates = [];
+    this.selected = [];
     this.service.processFolder(this.state.foldername, this.scoreConfig).subscribe(res => {
       this.candidates = res['candidates'];
       this.rescore();
+      this.hasToc = true;
       this.loading = false;
     });
   }
 
   export() {
     this.loading = true;
-    let selected: Candidate[] = this.candidates.filter((c:Candidate) => {return c['selected']});
-    this.service.export(selected).subscribe(res => {
-      console.log(res);
+    this.selected = this.candidates.filter((c: Candidate) => { return c['selected'] });
+    this.service.export(this.selected).subscribe(res => {
+      this.exported = JSON.stringify(res);
+      this.exportModal.open();
       this.loading = false;
-    });
-  }
-
-  modalOpened() {
-    this.service.getTocText(this.state.foldername).subscribe(res => {
-      this.toc_text = res;
     });
   }
 
@@ -71,15 +81,15 @@ export class HomeComponent implements OnInit {
         if (c.text.split(" ").length > 1) {
           c.score = c.score * sc.multiple;
         }
-        
+
         for (let i in c.dictionaries) {
           let dm = c.dictionaries[i];
           if (sc.dictionaries.hasOwnProperty(dm)) {
             c.score = c.score * sc.dictionaries[dm];
           }
         }
-      
-      
+
+
       }
       if (c.hasProperNoun) {
         c.score = c.score * sc.hasProperNoun;
@@ -98,15 +108,12 @@ export class HomeComponent implements OnInit {
     console.log(e, d);
   }
 
-
-  public modalOptions: Materialize.ModalOptions = {
-    ready: (modal, trigger) => { // Callback for Modal open. Modal and trigger parameters available.
-      
-      this.service.getTocText(this.state.foldername).subscribe(res => {
-        this.toc_text = res;
-      });
-      console.log(modal, trigger);
-    }
-  };
+  openToc() {
+    this.service.getTocText(this.state.foldername).subscribe(res => {
+      this.toc_text = res;
+      this.tocModal.open();
+      this.loading = false;
+    });
+  }
 
 }
